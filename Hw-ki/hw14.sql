50364dab4ce1e5678eb63d0d18b6a876fd12e6d1
AsAnna <anya.shaman@gmail.com>
2025-03-18 18:46:21 +0300

    little change hw14

--- Hw-ki/hw14.sql
+++ Hw-ki/hw14.sql
@@ -2,14 +2,14 @@
 --● 1Создайте таблицу "Employees" с указанными полями.
 CREATE TABLE Employees (
     id          SERIAL PRIMARY KEY,
-    id_name        VARCHAR(50),
+    name        VARCHAR(50),
     position    VARCHAR(30),
     department  VARCHAR(30),
     salary      DECIMAL(10,2)
 );
 
 --● 2Вставьте в таблицу несколько записей с информацией о сотрудниках вашей компании.
-INSERT INTO employees (id_name, position, department, salary)
+INSERT INTO employees (name, position, department, salary)
 VALUES 
 ('Иван Иванов', 'Manager', 'Sales', 5000),
 ('Мария Петрова', 'Разработчик', 'IT', 6000),
@@ -27,23 +27,23 @@ VALUES
 UPDATE employees 
 SET position = 'Fullstack-разработчик', 
 salary = 8000 
-WHERE id_name = 'Мария Петрова';
+WHERE name = 'Мария Петрова';
 
 --● 4Добавьте новое поле "HireDate" (дата приема на работу) в таблицу "Employees".
 ALTER TABLE employees ADD COLUMN hire_date DATE;
 
 --● 5Добавьте записи о дате приема на работу для всех сотрудников.
-UPDATE employees SET hire_date = '2025.03.16';
-UPDATE employees SET hire_date = '2025.01.28' WHERE department IN ('Sales', 'IT', 'Кадры');
+UPDATE employees SET hire_date = '2025-03-16';
+UPDATE employees SET hire_date = '2025-01-28' WHERE department IN ('Sales', 'IT', 'Кадры');
 
 --● 6Найдите всех сотрудников, чья должность "Manager".
-SELECT id_name FROM employees WHERE position = 'Manager';
+SELECT name FROM employees WHERE position = 'Manager';
 
 --● 7Найдите всех сотрудников, у которых зарплата больше 5000 долларов.
-SELECT id_name FROM employees WHERE salary > 5000;
+SELECT name FROM employees WHERE salary > 5000;
 
 --● 8Найдите всех сотрудников, которые работают в отделе "Sales".
-SELECT id_name FROM employees WHERE department = 'Sales';
+SELECT name FROM employees WHERE department = 'Sales';
 
 --● 9Найдите среднюю зарплату по всем сотрудникам.
 SELECT AVG(salary) AS avg_salary FROM employees;
@@ -56,23 +56,23 @@ DROP TABLE employees;
 
 --● 6Найдите всех сотрудников, чья должность "Manager".
 CREATE OR REPLACE FUNCTION get_position(x_position VARCHAR) 
-RETURNS TABLE (get_id_name VARCHAR) AS $$
+RETURNS TABLE (get_name VARCHAR) AS $$
 BEGIN
     RETURN QUERY  -- из-за отсутствия этой строки долгое время не получалось вывести результат
-    SELECT id_name FROM employees WHERE position ILIKE x_position;
+    SELECT name FROM employees WHERE position ILIKE x_position;
 END;
 $$ 
 LANGUAGE plpgsql;
--- DROP ROUTINE get_position(VARCHAR);  -- любимый запрос ;)
+--DROP ROUTINE get_position(VARCHAR);  -- любимый запрос ;)
 SELECT * FROM get_position('Manager'); 
 
 
 --● 7Найдите всех сотрудников, у которых зарплата больше 5000 долларов.
 CREATE OR REPLACE FUNCTION get_salary(DECIMAL) 
-RETURNS TABLE (get_id_name VARCHAR) AS $$
+RETURNS TABLE (get_name VARCHAR) AS $$
 BEGIN
     RETURN QUERY
-    SELECT id_name FROM employees WHERE salary > $1;
+    SELECT name FROM employees WHERE salary > $1;
 END;
 $$ 
 LANGUAGE plpgsql;
@@ -81,10 +81,10 @@ SELECT * FROM get_salary(5000);
 
 --● 8Найдите всех сотрудников, которые работают в отделе "Sales".
 CREATE OR REPLACE FUNCTION get_department(VARCHAR) 
-RETURNS TABLE (get_id_name VARCHAR) AS $$
+RETURNS TABLE (get_name VARCHAR) AS $$
 BEGIN
     RETURN QUERY  
-    SELECT id_name FROM employees WHERE department = $1;
+    SELECT name FROM employees WHERE department = $1;
 END;
 $$ 
 LANGUAGE plpgsql;

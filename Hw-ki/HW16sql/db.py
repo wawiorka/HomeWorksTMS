db3e8d7154a96c9acd697940355f874b1deb36bb
AsAnna <anya.shaman@gmail.com>
2025-03-30 15:37:13 +0300

    add hw16 - sql console mini app

---
+++ Hw-ki/HW16sql/db.py
@@ -0,0 +1,200 @@
+# Разработка схемы базы данных для мероприятий, мест и билетов
+
+import psycopg2
+
+DB_CONFIG = {
+    "dbname": "ticket_serv_db",
+    "user": "postgres",
+    "password": "3618ann",
+    "host": "127.0.0.1",
+    "port": "5432"
+}
+
+
+def connect_db():
+    return psycopg2.connect(**DB_CONFIG)
+
+
+def init_db():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE  IF NOT EXISTS events (
+                id SERIAL PRIMARY KEY, 
+                title VARCHAR(100) UNIQUE,
+                date VARCHAR(15)
+            );
+        """)
+
+        cur.execute("""
+            CREATE TABLE  IF NOT EXISTS plases (
+                id SERIAL PRIMARY KEY, 
+                name VARCHAR(100) UNIQUE,
+                nums_tickets INT
+            );
+        """)
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS events_plases (
+                id SERIAL PRIMARY KEY,
+                event_id INT REFERENCES events (id) ON DELETE CASCADE,
+                plase_id INT REFERENCES plases (id) ON DELETE CASCADE
+            );
+        """)
+
+        cur.execute("""
+            CREATE TABLE  IF NOT EXISTS tickets (
+                id SERIAL PRIMARY KEY,
+                buyer VARCHAR(20),
+                event_id INT REFERENCES events (id) ON DELETE CASCADE
+            );
+        """)
+    
+
+def content_db():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO events (title, date)
+                VALUES 
+                ('CodeFest: Хакатон для начинающих', '2025-04-16'),
+                ('DevTalks: Встречи с профессионалами IT', '2025-04-18'),
+                ('Algorhythm: Конкурс алгоритмических решений', '2025-05-16'),
+                ('TechLab: Мастер-классы по новым технологиям', '2025-07-16'),
+                ('Hack & Learn: Обучающие сессии по программированию', '2025-06-06'),
+                ('Open Source Jam: Создание проектов с нуля', '2025-07-08'),
+                ('Debugging Day: Поиск и исправление ошибок в коде', '2025-08-10'),
+                ('Game Dev Challenge: Создание игр за 48 часов', '2025-05-26'),
+                ('AI Bootcamp: Погружение в искусственный интеллект', '2025-05-30'),
+                ('Code Review Night: Обсуждение и анализ кода', '2025-06-17');
+        """)
+
+        cur.execute("""
+            INSERT INTO plases (name, nums_tickets)
+                VALUES 
+                ('Малый зал кинотеатра', 100),
+                ('Концертный зал', 150),
+                ('Конференц-холл главной арены', 50),
+                ('Актовый зал университета', 50),
+                ('Главная библиотека', 30)
+        """)
+
+        cur.execute("""
+            INSERT INTO events_plases (event_id,plase_id)
+                VALUES
+                (1, 4),
+                (2, 2),
+                (3, 5),
+                (4, 3),
+                (5, 1),
+                (6, 3),
+                (7, 5),
+                (8, 1),
+                (9, 4),
+                (10, 4);
+        """)
+
+def get_events():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM events")
+        events = cur.fetchall()
+        return events
+    
+def get_plases():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM plases")
+        plases = cur.fetchall()
+        return plases
+
+
+def get_plases_events():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("""SELECT p.name, p.id, e.title, e.date
+                        FROM events_plases AS e_p
+                        JOIN plases AS p ON p.id = e_p.plase_id
+                        JOIN events AS e ON e.id = e_p.event_id;""")
+        plases_ev = cur.fetchall()
+        return plases_ev
+    
+
+def add_event(title, date):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("INSERT INTO events (title, date) VALUES (%s, %s)", (title, date,))
+
+
+def add_in_events_plases(id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("INSERT INTO events_plases (event_id) VALUES (%s)", [(id,)])
+
+
+def add_plase(name):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("INSERT INTO plases (name) VALUES (%s)", (name,))
+
+
+def delete_event(id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.executemany("DELETE FROM events WHERE id = %s", [(id,)])
+
+
+def delete_plase(id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.executemany("DELETE FROM plases WHERE id = %s", [(id,)])
+
+
+
+def update_event(title, date, id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("UPDATE events SET title = (%s), date = (%s) WHERE id = %s", (title, date, id, ))
+
+def update_plase(plase_id, ivent_id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("UPDATE events_plases SET plase_id = %s WHERE event_id = %s", (plase_id, ivent_id, ))
+
+
+def reserv_ticket(buyer, ivent_id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("INSERT INTO tickets (buyer, event_id) VALUES (%s, %s)", (buyer, ivent_id,))
+        # t_n = cur.execute("SELECT COUNT(buyer) AS t_n FROM tickets WHERE event_id = %s", (ivent_id, ))
+        # print(type(t_n))
+        # print(f"Количество проданных билетов - {int(t_n)}")
+
+
+def return_ticket(id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.executemany("DELETE FROM tickets WHERE id = %s", [(id,)])
+
+
+def search_events(query):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM events WHERE title ILIKE %s;", (f"%{query}%",))
+        events = cur.fetchall()
+        return events
+    
+
+def search_plases(query):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("SELECT * FROM plases WHERE name ILIKE %s;", (f"%{query}%",))
+        plases = cur.fetchall()
+        return plases
+    
+
+def get_num_tickets(id):
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("SELECT COUNT(id) FROM tickets WHERE event_id = %s", (id,))
+        num_tickets = cur.fetchall()
+        
+        cur.execute("SELECT title FROM events WHERE id = %s", (id,))
+        event = cur.fetchall()
+
+        return {
+            "num_tickets": num_tickets,
+            "event": event
+        }
+
+def get_tickets():
+    with connect_db() as conn, conn.cursor() as cur:
+        cur.execute("""SELECT t.id, t.buyer, e.title, e.date
+                        FROM tickets AS t
+                        JOIN events AS e ON e.id = t.event_id;""")
+        tickets_info = cur.fetchall()
+        return tickets_info
+    
---
+++ Hw-ki/HW16sql/main.py
@@ -0,0 +1,227 @@
+"""Вариант 4 - Сервис резервирования билетов:
+Функционал:
+• Разработка схемы базы данных для мероприятий, мест и билетов
+• Простые операции, такие как добавление/удаление/редактирование
+событий/мест, вывод информации о событии/месте по названию
+• Сложные операции, такие как поиск события по месту, поиск
+события/курса/whatever по частичному совпадению названия
+• Функционал для бронирования и отмены билетов"""
+
+import db
+
+
+def print_menu():
+    print("\nЧто вы хотите сделать? (выберите нужную команду) ")
+    print("0. Выход")
+    print("1. Посмотреть список мероприятий")
+    print("2. Посмотреть список мест")
+    print("3. Посмотреть список мест и мероприятий")
+    print("4. Изменить список мероприятий")
+    print("41. Добавить новое мероприятие в список связей")
+    print("5. Изменить список мест")    
+    print("6. Изменить место проведения мероприятия")
+    print("7. Поиск объектов по названию")
+    print("8. Поиск объектов по части названия")
+    print("9. Бронирование билетов")
+    print("10. Возврат билетов")
+    print("11. Вывод информации о числе забронированных билетов по ID мероприятия")
+    print("12. Вывод информации всех забронированных билетах")
+
+        
+def app():
+    print("Добрый день! Вы запустили Сервис Резервиравания Билетов")
+    
+    db.init_db()
+    print("Таблицы созданы")
+    
+    # отлов ошибки на дозаписывание новых строк при повторном запуске, получше варианта не придумала
+    try:
+        db.content_db()
+    except Exception as e:
+        print(f"Что-то пошло не так! {e}")
+
+    while True:
+        print_menu()
+        cmd = int(input("Введите номер команды: "))
+
+        if cmd == 0:
+            print("\nСпасибо за пользование сервисом! До свидания!")
+            break
+        
+        elif cmd == 1:  # Показать список мероприятий
+            print("\nСписок мероприятий: ")
+            events = db.get_events()
+            for event in events:
+                print(f"ID {event[0]} - {event[1]}, {event[2]}.")
+
+        elif cmd == 2:  # Показать список мест
+            print("\nСписок мест: ")
+            plases = db.get_plases()
+            for plase in plases:
+                print(f"ID {plase[0]} - {plase[1]}.")
+        
+        elif cmd == 3:  # Показать список мест и мероприятий
+            print("\nСписок: ")
+            plases_ev = db.get_plases_events()
+            for plase in plases_ev:
+                print(f"{plase[0]} (id {plase[1]}) - {plase[2]}, {plase[3]}.")
+        
+        elif cmd == 4:  # Изменить список мероприятий
+            print("\nЧто вы хотите сделать? (выберите нужную команду)")
+            print("1. добавить мероприятие.")
+            print("2. удалить мероприятие.")
+            print("3. отредактировать мероприятие.")
+            cmd3 = int(input("Введите команду "))
+            if cmd3 == 1:
+                print("\nВы выбрали добавление мероприятия")
+                title = input("Введите название мероприятия: ")
+                date = input("Введите дату мероприятия: ")
+                try:
+                    db.add_event(title, date)
+                    print("Мероприятие добавлено.")
+                except Exception as e:
+                    print(f"Что-то пошло не так! {e}")
+            elif cmd3 == 2:
+                print("\nВы выбрали удаление мероприятия")
+                id = input("Введите ID удаляемого мероприятия: ")
+                try:
+                    db.delete_event(id)
+                    print("Мероприятие удалено.")
+                except Exception as e:
+                    print(f"Что-то пошло не так! {e}")
+            elif cmd3 == 3:
+                print("\nВы выбрали обновление мероприятия")
+                id = input("Введите ID обновляемого мероприятия: ")
+                title = input("Введите новое название мероприятия: ")
+                date = input("Введите новую дату мероприятия: ")
+                try:
+                    db.update_event(title, date, id)
+                    print("Информация о мероприятии обновлена.")
+                except Exception as e:
+                    print(f"Что-то пошло не так! {e}")
+            else:
+                print("Вы ввели несуществующею команду. Попробуйте еще раз!")
+            
+        elif cmd == 41:  # Добавить новое мероприятие в список связей
+            print("\nВы выбрали добавление нового мероприятия в список связей")
+            print("\nСписок мероприятий: ")
+            events = db.get_events()
+            for event in events:
+                print(f"ID {event[0]} - {event[1]}, {event[2]}.")
+            id_ev_pl = input("Введите ID мероприятия: ")
+            try:
+                db.add_in_events_plases(id_ev_pl)
+                print("Мероприятие добавлено.")
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+
+        elif cmd == 5:  # Изменить список меcт
+            print("\nЧто вы хотите сделать? (выберите нужную команду)")
+            print("1. добавить место.")
+            print("2. удалить место.")
+            cmd3 = int(input("Введите команду "))
+            if cmd3 == 1:
+                print("\nВы выбрали добавление места")
+                plase_name = input("Введите название места: ")
+                try:
+                    db.add_plase(plase_name)
+                    print("Место добавлено.")
+                except Exception as e:
+                    print(f"Что-то пошло не так! {e}")
+            elif cmd3 == 2:
+                print("\nВы выбрали удаление места")
+                id = input("Введите ID удаляемого места: ")
+                try:
+                    db.delete_plase(id)
+                    print("Место удалено.")
+                except Exception as e:
+                    print(f"Что-то пошло не так! {e}")
+            else:
+                print("Вы ввели несуществующею команду. Попробуйте еще раз!")
+
+        elif cmd == 6: # Изменить место проведения мероприятия
+            print("\nВы выбрали обновление мероприятия")
+            ivent_id = input("\nВведите ID мероприятия, место проведения которого хотите поменять: ")
+            plase_id = input("Введите ID места проведения мероприятия: ")
+            try:
+                db.update_plase(plase_id, ivent_id)
+                print("Информация о месте проведения мероприятия обновлена.")
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+        
+        elif cmd == 7:  # Поиск объектов по названию
+            query = input("\nЧто вы ищете (введите название): ")
+            with db.connect_db() as conn, conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM events WHERE title = '{query}';")
+                events = cur.fetchall()
+            print(f"Найдено мероприятий по запросу:")
+            for event in events:
+                print(f"ID {event[0]} - {event[1]}.")
+            with db.connect_db() as conn, conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM plases WHERE name = '{query}';")
+                plases = cur.fetchall()
+            print(f"Найдено меcт по запросу:")
+            for plase in plases:
+                print(f"ID {plase[0]} - {plase[1]}.")
+        
+        elif cmd == 8:  # Поиск объектов по части названия
+            query = input("\nЧто вы ищете (введите название/часть названия): ")
+            events = db.search_events(query)
+            plases = db.search_plases(query)
+            print(f"Найдено мероприятий по запросу '{query}':")
+            for event in events:
+                print(f"ID {event[0]} - {event[1]}.")
+            print(f"Найдено меcт по запросу '{query}':")
+            for plase in plases:
+                print(f"ID {plase[0]} - {plase[1]}.")
+        
+        # • Функционал для бронирования и отмены билетов"""
+        elif cmd == 9:  # бронирование билетов
+            print("\nВы выбрали бронирование билетов")
+            ivent_id = input("Введите ID мероприятия, куда хотите забронировать билет: ")
+            buyer = input("Введите ваше имя для покупки билета: ") 
+            try:
+                db.reserv_ticket(buyer, ivent_id)
+                print("Билет успешно прибретен.")
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+        
+        elif cmd == 10:  # возврат билетов
+            print("\nВы выбрали возврат билетов")
+            id = input("Введите ID билета: ")
+            try:
+                db.return_ticket(id)
+                print("Билет успешно анулирован.")
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+        
+        elif cmd == 11:  # Вывод информации о числе забронированных билетов по ID мероприятия
+            print("\nВывод информации о числе забронированных билетов на мероприятие")
+            id = input("Введите ID мероприятия: ")
+            try:
+                event_tickets = db.get_num_tickets(id)
+                num_tickets = event_tickets["num_tickets"]
+                event = event_tickets["event"]   
+                for ev in event:
+                    print(f"На мероприятие: '{ev[0]}':")                   
+                for ticket in num_tickets:
+                    print(f"Забронировано билетов: {ticket[0]}")
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+        
+        elif cmd == 12:  # Вывод информации о числе забронированных билетов на мероприятие
+            print("\nВывод информации о числе забронированных билетов на мероприятие")
+            try:
+                all_tickets = db.get_tickets()
+                
+                for t in all_tickets:
+                    print(f'Уникальный номер билета: {t[0]}; Покупатель - {t[1]}; Название: {t[2]}, {t[3]}.')
+                
+            except Exception as e:
+                print(f"Что-то пошло не так! {e}")
+
+        else:
+            print("Вы ввели несуществующею команду. Попробуйте еще раз!")
+        
+
+app()
